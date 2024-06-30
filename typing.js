8046f48834c668ccdd93d72809aec622584e9fdf
jpsaha <jpsaha@users.noreply.github.com>
2024-06-30 03:41:28 +0000

    Deploying to gh-pages from @ jpsaha/MOTP@f214655d3e74dde7df4022abebcec71e42144e95 🚀

---
+++ typing.js
@@ -0,0 +1,1 @@
+window.onload=function(){new Typed("#typing-text",{strings:["Algebra","Combinatorics","Geometry","Number Theory"],typeSpeed:50,backDelay:2e3,fadeOut:!1,backSpeed:40,smartBackspace:!0,loop:!0,showCursor:!1})};
