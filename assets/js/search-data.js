00c103962f4710c6df09c6b106bf0d8699b992a4
jpsaha <jpsaha@users.noreply.github.com>
2026-05-25 17:26:01 +0000

    Deploying to gh-pages from @ jpsaha/MOTP@79c12c7440e02e8d7092fef80fc0beb739006dfe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -86,6 +86,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/MOTP/mopss/";
           },
+        },{id: "nav-mopss",
+          title: "MOPSS",
+          description: "Mathematics Olympiad Problem Solving Sessions",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/MOTP/MOPSS/";
+          },
         },{id: "nav-pyqs",
           title: "PYQs",
           description: "Previous Years&#39; Question Papers of Pre-RMO, IOQM, RMO, INMO, EGMOTC, IMOTC. Problem set, Solutions, Questions, Answers, Hints, Walkthroughs, Discussions, Solutions in pdf.",
